aa0435cea9ed82eecd88ad953f4e6d1a378e6706
Marc Sluiter <marc.sluiter@luminis.eu>
2015-07-10 15:31:17 +0200

    Removed docker registry, load images on each node instead. Pre-download and provision binaries. Fixed cloud-init for better startup order and reboots.
    don't enable systemd units, bc that causes the unit to start at a reboot before the configuration from cloud-init is applied

---
+++ Controller/bin/initial-download.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+weave_version=0.9.0
+k8s_version=v0.18.2
+pause_version=0.8.0
+
+# get absolute path of this script
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+for NAME in celix-agent felix-agent node-provisioning; do
+	echo "building and saving $NAME image"
+	docker build -t "inaetics/$NAME:latest" "$DIR/../inaetics-demo/$NAME/"
+	docker save -o "$DIR/../inaetics-demo/images/$NAME.tar" "inaetics/$NAME:latest"
+done
+
+echo "pulling and saving weave images"
+weave_name="zettio/weave:$weave_version"
+weavetools_name="zettio/weavetools:$weave_version"
+docker pull "$weave_name"
+docker save -o "$DIR/../images/weave.tar" "$weave_name"
+docker pull "$weavetools_name"
+docker save -o "$DIR/../images/weavetools.tar" "$weavetools_name"
+
+echo "downloading weave scripts"
+wget -N -P "$DIR/../opt/bin" "https://github.com/weaveworks/weave/releases/download/v$weave_version/weave"
+wget -N -P "$DIR/../opt/bin" "https://raw.githubusercontent.com/slintes/weave-demos/master/poseidon/weave-helper"
+
+echo "downloading kubernetes binaries"
+wget -O - "https://storage.googleapis.com/kubernetes-release/release/$k8s_version/kubernetes-server-linux-amd64.tar.gz" | tar -xz -C "$DIR/../opt/bin" --strip=3
+rm "$DIR"/../opt/bin/*.docker_tag
+rm "$DIR"/../opt/bin/*.tar
+
+echo "pulling and saving pause image"
+pause_name="gcr.io/google_containers/pause:$pause_version"
+docker pull "$pause_name"
+docker save -o "$DIR/../images/pause.tar" "$pause_name"
+
--- Controller/bin/load-docker-images.sh
+++ Controller/bin/load-docker-images.sh
@@ -4,20 +4,10 @@ cd /home/core/inaetics-demo/images
 for I in *.tar; do
 	echo "loading image $I"
 	docker load -i $I
-	name="localhost:5000/inaetics/${I%.*}:latest"
-	echo "pushing image $name"
-	docker push $name &
 done
 
 cd /home/core/images
-for I in pause.tar weave.tar weavetools.tar; do
+for I in *.tar; do
 	echo "loading image $I"
 	docker load -i $I
 done
-
-echo "pushing kubernetes pause image"
-docker push localhost:5000/google_containers/pause:0.8.0 &
-
-echo "pushing weave images"
-docker push localhost:5000/zettio/weave:0.9.0 &
-docker push localhost:5000/zettio/weavetools:0.9.0 &
