20b03c09ec6781295750202a6147565f52bcecdf
Marc Sluiter <marc.sluiter@luminis.eu>
2015-08-18 19:01:16 +0200

    use flannel instead of weave

--- Controller/bin/initial-download.sh
+++ Controller/bin/initial-download.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-weave_version=0.9.0
 k8s_version=v1.0.3
 pause_version=0.8.0
 
@@ -12,18 +11,6 @@ for NAME in celix-agent felix-agent node-provisioning; do
 	docker build -t "inaetics/$NAME:latest" "$DIR/../inaetics-demo/$NAME/"
 	docker save -o "$DIR/../inaetics-demo/images/$NAME.tar" "inaetics/$NAME:latest"
 done
-
-echo "pulling and saving weave images"
-weave_name="zettio/weave:$weave_version"
-weavetools_name="zettio/weavetools:$weave_version"
-docker pull "$weave_name"
-docker save -o "$DIR/../images/weave.tar" "$weave_name"
-docker pull "$weavetools_name"
-docker save -o "$DIR/../images/weavetools.tar" "$weavetools_name"
-
-echo "downloading weave scripts"
-wget -N -P "$DIR/../opt/bin" "https://github.com/weaveworks/weave/releases/download/v$weave_version/weave"
-wget -N -P "$DIR/../opt/bin" "https://raw.githubusercontent.com/slintes/weave-demos/master/poseidon/weave-helper"
 
 echo "downloading kubernetes binaries"
 wget -O - "https://storage.googleapis.com/kubernetes-release/release/$k8s_version/kubernetes-server-linux-amd64.tar.gz" | tar -xz -C "$DIR/../opt/bin" --strip=3
