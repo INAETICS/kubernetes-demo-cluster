596bf4a9f3058e31caa39de51e3d46cd57cf5e58
Marc Sluiter <marc.sluiter@luminis.eu>
2015-10-07 19:22:07 +0200

    cleaned up folder structure for less vagrant provisioning (see below), added docker registry to controller node for inaetics images
    - moved all scripts which are needed during runtime to /opt/bin (can be called without path now)
    - separated images needed on all nodes and on controller node only to 2 folders
    - inaetics-demo folder only contains inaetics docker images and does not need to be provisioned anymore

--- Controller/bin/create-k8s-replicationControllers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export $(cat /etc/kubernetes.env)
-/opt/bin/kubectl create -f /home/core/inaetics-demo/k8s/ace-provisioning-controller.json
-/opt/bin/kubectl create -f /home/core/inaetics-demo/k8s/inaetics-datastore-viewer-controller.json
-/opt/bin/kubectl create -f /home/core/inaetics-demo/k8s/inaetics-processor-controller.json
-/opt/bin/kubectl create -f /home/core/inaetics-demo/k8s/inaetics-producer-controller.json
-/opt/bin/kubectl create -f /home/core/inaetics-demo/k8s/inaetics-queue-controller.json
