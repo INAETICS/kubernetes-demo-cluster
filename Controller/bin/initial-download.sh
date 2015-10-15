ae97109cc5860e770e9d334e4d35708efcd9df73
Marc Sluiter <marc.sluiter@luminis.eu>
2015-10-15 15:10:26 +0200

    some fixes after rebasing

--- Controller/bin/initial-download.sh
+++ Controller/bin/initial-download.sh
@@ -19,8 +19,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # build inaetics images
 for NAME in celix-agent felix-agent node-provisioning; do
 	echo "pulling and saving $NAME image"
-	docker pull "inaetics/$NAME:latest"
-	docker save -o "$DIR/../inaetics-demo/images/$NAME.tar" "inaetics/$NAME:latest"
+	remote_name="inaetics/$NAME:latest"
+	local_name="172.17.8.20:5000/$remote_name"
+	docker pull "$remote_name"
+	docker tag -f "$remote_name" "$local_name"
+	docker save -o "$DIR/../inaetics-demo/images/$NAME.tar" "$local_name"
 done
 
 echo "get latest bundles"
